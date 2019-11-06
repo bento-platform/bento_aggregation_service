f8953f49e37962a6b768ade371de7aabc6b4cf80
David Lougheed <david.lougheed@gmail.com>
2019-11-06 10:57:07 -0500

    Add check for CHORD_URL on boot with run.py

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ import tornado.ioloop
 from chord_federation.app import application
 
 if __name__ == "__main__":
+    if os.environ.get("CHORD_URL", None) is None:
+        print("[CHORD Federation] No CHORD URL given, terminating...")
+        exit(1)
+
     application.listen(int(os.environ.get("PORT", "5000")))
     tornado.ioloop.IOLoop.instance().start()
