8eaebce4ad2a736a402220dbffe5d7de6dc4f4b4
David Lougheed <david.lougheed@gmail.com>
2020-01-10 13:48:37 -0500

    Use urljoin for peer_fetch

--- chord_federation/utils.py
+++ chord_federation/utils.py
@@ -2,6 +2,7 @@ import json
 
 from tornado.httpclient import AsyncHTTPClient
 from typing import Optional
+from urllib.parse import urljoin
 
 from .constants import TIMEOUT
 
@@ -12,7 +13,7 @@ __all__ = ["peer_fetch"]
 async def peer_fetch(client: AsyncHTTPClient, peer: str, path_fragment: str, request_body: Optional[bytes] = None,
                      method: str = "POST", extra_headers: Optional[dict] = None):
     r = await client.fetch(
-        f"{peer}{path_fragment}",
+        urljoin(peer, path_fragment),
         request_timeout=TIMEOUT,
         method=method,
         body=request_body,
