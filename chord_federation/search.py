98937053588dd40d822377b973d7d5cbdbc49f5e
David Lougheed <david.lougheed@gmail.com>
2020-01-15 08:52:31 -0500

    Fix search when there's neither matches nor a join query

--- chord_federation/search.py
+++ chord_federation/search.py
@@ -225,17 +225,19 @@ class DatasetSearchHandler(RequestHandler):  # TODO: Move to another dedicated s
             print("[CHORD Federation {}] Done fetching individual service search results.".format(datetime.now()),
                   flush=True)
 
-            for d, s in dataset_objects_dict.items():  # TODO: Worker
-                # d: dataset identifier
-                # s: dict of data types and corresponding table matches
+            for dataset_id, data_type_results in dataset_objects_dict.items():  # TODO: Worker
+                # dataset_id: dataset identifier
+                # data_type_results: dict of data types and corresponding table matches
                 # Append result if:
                 #  - No join query was specified and there is at least one matching table present in the dataset; or
                 #  - A join query is present and evaluates to True against the dataset.
                 # Need to mark this query as internal, since the federation service "gets" extra privileges here
                 # (joined data isn't explicitly exposed.)
-                if ((join_query_ast is None and any(len(dtr) > 0 for dtr in s.values())) or
-                        check_ast_against_data_structure(join_query_ast, s, dataset_object_schema, internal=True)):
-                    results.append(datasets_dict[d])  # TODO: Make sure all information here is public-level.
+                if ((join_query_ast is None and any(len(dtr) > 0 for dtr in data_type_results.values())) or
+                        (join_query_ast is not None and
+                         check_ast_against_data_structure(join_query_ast, data_type_results, dataset_object_schema,
+                                                          internal=True))):
+                    results.append(datasets_dict[dataset_id])  # TODO: Make sure all information here is public-level.
 
             self.write({"results": results})
 
