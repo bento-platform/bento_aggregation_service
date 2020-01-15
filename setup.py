7caffa1a8a6f497ca5996c95b33e735fc020fa0e
David Lougheed <david.lougheed@gmail.com>
2020-01-15 15:40:32 -0500

    Update / better specify dependencies; freeze chord_lib

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     version="0.1.0",
 
     python_requires=">=3.6",
-    install_requires=["chord_lib @ git+https://github.com/c3g/chord_lib", "tornado"],
+    install_requires=["chord_lib==0.1.0", "tornado>=6.0,<6.1"],
 
     author="David Lougheed",
     author_email="david.lougheed@mail.mcgill.ca",
