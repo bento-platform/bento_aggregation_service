abd59dbe0ae65fb231726d32d96e9e41b6ca46e0
David Lougheed <david.lougheed@gmail.com>
2020-03-05 16:11:28 -0500

    Update chord_lib to 0.6.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     version="0.3.0",
 
     python_requires=">=3.6",
-    install_requires=["chord_lib==0.5.0", "tornado>=6.0,<6.1"],
+    install_requires=["chord_lib==0.6.0", "tornado>=6.0,<6.1"],
 
     author="David Lougheed",
     author_email="david.lougheed@mail.mcgill.ca",
