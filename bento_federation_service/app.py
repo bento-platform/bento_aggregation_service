bba2c923c65336a8fc345cd392787fcd3a8fda10
Julian <ornatus30@gmail.com>
2022-11-22 14:27:00 -0500

    add git version information to federation service

--- bento_federation_service/app.py
+++ bento_federation_service/app.py
@@ -3,6 +3,10 @@ import tornado.gen
 import tornado.ioloop
 import tornado.web
 
+import subprocess
+import sys
+import os
+
 from datetime import datetime
 from tornado.httpserver import HTTPServer
 from tornado.netutil import bind_unix_socket
@@ -17,6 +21,7 @@ from .constants import (
     CHORD_URLS_SET,
     BASE_PATH,
     SERVICE_SOCKET,
+    CHORD_DEBUG
 )
 from .db import peer_db
 from .peers.handlers import PeerHandler, PeerRefreshHandler
@@ -26,24 +31,55 @@ from .search.dataset_search.handlers.private_dataset import PrivateDatasetSearch
 from .search.federated_dataset_search import FederatedDatasetsSearchHandler
 from .search.search import SearchHandler
 
+path_for_git = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+
+def before_first_request_func():
+    try:
+        subprocess.run(["git", "config", "--global", "--add", "safe.directory", str(path_for_git)])
+    except Exception as e:
+        except_name = type(e).__name__
+        print("Error in dev-mode retrieving git folder configuration", except_name)
+
+before_first_request_func()
 
 # noinspection PyAbstractClass,PyAttributeOutsideInit
 class ServiceInfoHandler(RequestHandler):
     async def get(self):
         # Spec: https://github.com/ga4gh-discovery/ga4gh-service-info
-        self.write({
+        info_service = {
             "id": SERVICE_ID,
             "name": SERVICE_NAME,  # TODO: Should be globally unique?
             "type": SERVICE_TYPE,
             "description": "Federation service for a Bento platform node.",
+            "environment": "prod",
             "organization": {
                 "name": "C3G",
                 "url": "http://www.computationalgenomics.ca"
             },
             "contactUrl": "mailto:david.lougheed@mail.mcgill.ca",
             "version": bento_federation_service.__version__
-        })
+        }
+        if CHORD_DEBUG:
+            info = {
+                **info_service,
+                "environment": "dev",
+                }
+            try:
+                res_tag = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+                if res_tag:
+                    info["git_tag"] = res_tag.decode().rstrip()
+                res_branch = subprocess.check_output(["git", "branch", "--show-current"])
+                if res_branch:
+                    info["git_branch"] = res_branch.decode().rstrip()
 
+            except Exception as e:
+                except_name = type(e).__name__
+                print("Error in dev-mode retrieving git information", except_name)
+
+            self.write(info)
+
+        else:
+            self.write(info_service)
 
 async def post_start_hook(peer_manager: PeerManager):
     if BENTO_FEDERATION_MODE:
