d2ae0fcc5a16351cc3ce3f7dfa809a186046d713
Julian <ornatus30@gmail.com>
2022-11-23 15:55:38 -0500

    Add git/environment info to aggregation service

--- bento_aggregation_service/app.py
+++ bento_aggregation_service/app.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os
+import subprocess
+
 import bento_aggregation_service
 import tornado.gen
 import tornado.ioloop
@@ -21,24 +24,56 @@ from .constants import (
 from .search.handlers.datasets import DatasetsSearchHandler
 from .search.handlers.private_dataset import PrivateDatasetSearchHandler
 
+path_for_git = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
+
+def before_first_request_func():
+    try:
+        subprocess.run(["git", "config", "--global", "--add", "safe.directory", str(path_for_git)])
+    except Exception as e:
+        except_name = type(e).__name__
+        print("Error in dev-mode retrieving git folder configuration", except_name)
+
+before_first_request_func()
 # noinspection PyAbstractClass,PyAttributeOutsideInit
+
 class ServiceInfoHandler(RequestHandler):
     async def get(self):
-        # Spec: https://github.com/ga4gh-discovery/ga4gh-service-info
-        self.write({
+        info_service = {
             "id": SERVICE_ID,
             "name": SERVICE_NAME,  # TODO: Should be globally unique?
             "type": SERVICE_TYPE,
             "description": "Aggregation service for a Bento platform node.",
+            "environment": "prod",
             "organization": {
                 "name": "C3G",
                 "url": "https://www.computationalgenomics.ca"
             },
             "contactUrl": "mailto:david.lougheed@mail.mcgill.ca",
             "version": bento_aggregation_service.__version__
-        })
+        }
+        if CHORD_DEBUG:
+            info = {
+                **info_service,
+                "environment": "dev",
+                }
+            try:
+                res_tag = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+                if res_tag:
+                    info["git_tag"] = res_tag.decode().rstrip()
+                res_branch = subprocess.check_output(["git", "branch", "--show-current"])
+                if res_branch:
+                    info["git_branch"] = res_branch.decode().rstrip()
 
+            except Exception as e:
+                except_name = type(e).__name__
+                print("Error in dev-mode retrieving git information", except_name)
+
+            self.write(info)
+
+        else:
+            # Spec: https://github.com/ga4gh-discovery/ga4gh-service-info
+            self.write(info_service)
 
 class Application(tornado.web.Application):
     def __init__(self, base_path: str):
