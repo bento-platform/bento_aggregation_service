2a5b296b87e7d779fb34ec020467f0441ca9789e
David Lougheed <david.lougheed@gmail.com>
2025-03-12 10:52:33 -0400

    chore!: remove unused /dataset-search endpoint BREAKING CHANGE: API endpoint removal

--- bento_aggregation_service/search/handlers/datasets.py
+++ bento_aggregation_service/search/handlers/datasets.py
@@ -30,51 +30,6 @@ __all__ = [
 dataset_search_router = APIRouter()
 
 
-async def search_worker(
-    # Input dataset list
-    datasets: list[dict],
-    # Input values
-    dataset_object_schema: dict,
-    join_query,
-    data_type_queries,
-    exclude_from_auto_join: tuple[str, ...],
-    # Dependencies
-    config: Config,
-    http_session: ClientSession,
-    logger: BoundLogger,
-    service_manager: ServiceManager,
-    headers: dict[str, str],
-    # Flags
-    include_internal_results: bool = False,
-):
-    async def _search_dataset(dataset: dict) -> tuple[str, dict[str, list] | None]:
-        dataset_id = dataset["identifier"]
-        try:
-            dataset_results = await run_search_on_dataset(
-                dataset_object_schema,
-                dataset,
-                join_query,
-                data_type_queries,
-                exclude_from_auto_join,
-                include_internal_results,
-                config,
-                http_session,
-                logger,
-                service_manager,
-                headers,
-            )
-            return dataset_id, dataset_results
-
-        except ClientResponseError as e:  # Thrown from run_search_on_dataset
-            # Metadata service error
-            # TODO: Better message
-            # TODO: Set error code outside worker?
-            await logger.aexception("error from dataset search", exc_info=e)
-            return dataset_id, None
-
-    return {**asyncio.gather(*(_search_dataset(ds) for ds in datasets))}
-
-
 class DatasetSearchRequest(BaseModel):
     # Format: {"data_type": ["#eq", ...]}
     data_type_queries: dict[str, Query]
@@ -84,79 +39,6 @@ class DatasetSearchRequest(BaseModel):
 
     # Format: list of data types to use as part of a full-join-ish thing instead of an inner-join-ish thing
     exclude_from_auto_join: tuple[str, ...] = ()
-
-
-@dataset_search_router.post("/dataset-search")
-async def all_datasets_search_handler(
-    request: Request,
-    search_req: DatasetSearchRequest,
-    config: ConfigDependency,
-    http_session: HTTPSessionDependency,
-    logger: LoggerDependency,
-    service_manager: ServiceManagerDependency,
-):
-    logger = logger.bind(search_req=search_req)
-
-    try:
-        # Try compiling each query to make sure it works. Any exceptions thrown will get caught below.
-        test_queries(search_req.data_type_queries.values())
-    except (TypeError, ValueError, SyntaxError) as e:  # errors from query processing
-        # TODO: Better message
-        await logger.aexception("query processing error", exc_info=e)
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Query processing error: {str(e)}")
-
-    results = []
-
-    try:
-        # TODO: Handle pagination
-        # TODO: Why fetch projects instead of datasets? Is it to avoid "orphan" datasets? Is that even possible?
-
-        await logger.adebug("fetching projects from Katsu")
-        headers = service_request_headers(request=request)
-        res = await http_session.get(
-            urljoin(config.katsu_url, "api/projects"),
-            headers=headers,
-            raise_for_status=True,
-        )
-
-        projects = await res.json()
-
-        datasets_dict: dict[str, dict] = {d["identifier"]: d for p in projects["results"] for d in p["datasets"]}
-
-        dataset_object_schema = {"type": "object", "properties": {}}
-
-        # Spawn workers to handle asynchronous requests to various datasets
-        dataset_objects_dict = await search_worker(
-            # dataset dictionaries:
-            list(datasets_dict.values()),
-            # search request / query-related:
-            dataset_object_schema,
-            search_req.join_query,
-            search_req.data_type_queries,
-            search_req.exclude_from_auto_join,
-            # dependencies:
-            config,
-            http_session,
-            logger,
-            service_manager,
-            headers,
-        )
-
-        await logger.ainfo("done fetching individual service search results")
-
-        # Aggregate datasets into results list if they satisfy the queries
-        for dataset_id, dataset_results in dataset_objects_dict.items():
-            if len(dataset_results) > 0:
-                d = datasets_dict[dataset_id]
-                results.append({**d, "results": {}})
-
-        return {"results": results}
-
-    except ClientResponseError as e:
-        # Metadata service error
-        # TODO: Better message
-        await logger.aexception("error from service", exc_info=e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error from service: {str(e)}")
 
 
 @dataset_search_router.post("/dataset-search/{dataset_id}")
